5937cebd9aaa49c768282e243bf0d37c508c9b82
sundayong <1137353877@qq.com>
2019-04-27 20:27:04 +0800

    痒痒鼠 v2.0 新增图片识别位置

---
+++ v2.0/Mitama/single/py/main.py
@@ -0,0 +1,31 @@
+import sys
+import time
+
+from util.HwndList import get
+from explore.py.Loop import loop
+
+def main():
+    print("欢迎来到阴阳师联盟！")
+    arg = 0
+    tt = 2
+    if sys.argv.__len__() > 1:  #多人组队
+        arg = sys.argv[1]
+    else:
+        tt = 1
+    #获取所有阴阳师句柄
+    list = get()
+    while True:
+        time.sleep(tt)  # 设置隔2秒运行一次
+
+        #循环所有句柄
+        for hd in list:
+            loop(hd)
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
